ed9286b960e75224d4de260677a6661fe91bbe57
Vinicius Scala Oliveira Brito <vsob@WINDOWS.CIN.UFPE.BR>
2019-10-22 14:57:27 -0300

    Comecei a fazerr a classe

---
+++ lista4-graph/cmake-build-debug/CMakeFiles/lista4_graph.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lista4_graph.dir/main.cpp.obj"
+  "lista4_graph.pdb"
+  "lista4_graph.exe"
+  "lista4_graph.exe.manifest"
+  "liblista4_graph.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lista4_graph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- lista4-graph/main.cpp
+++ lista4-graph/main.cpp
@@ -4,9 +4,22 @@
 using namespace std;
 
 
-class graph{
-    vector<vector<int>> teste;
+class sistemaOperacional{
+    vector<vector<int>> processos;//arestas que saem de processos
+    vector<int> recursos;//arestas que saem de recursos (Só pode ter 1 por recurso)
+
+    void req(int p, int r){
+
+    }
+
+    void fre(){
+
+    }
+
+
 };
+
+//o recurso só aponta para um lugar, ou seja, o que está usando ele
 
 int main(int argc, char *argv[]) {
     ios::sync_with_stdio(false);
