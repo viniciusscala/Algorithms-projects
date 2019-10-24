036b97cb055f7d9ce81c8208ad6870804d36b58f
Vinicius Scala Oliveira Brito <vsob@WINDOWS.CIN.UFPE.BR>
2019-10-24 10:52:39 -0300

    dps do bug da string

--- lista4-graph/main.cpp
+++ lista4-graph/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <string>
 #define endl '\n'
 
 using namespace std;
@@ -11,36 +12,43 @@ public:
     public:
         Vertice* val;
         Line* next;
+        int size;
 
         Line(){
             this->val = nullptr;
             this->next = nullptr;
+            this->size = 0;
         }
 
         Line(Vertice* node){
             this->val= node;
             this->next = nullptr;
+            this->size = 0;
         }
 
-        void in(Vertice* node){//vou botar no final
+        void in(Vertice* node, int* tam){//vou botar no final
             if(this->next==nullptr){
                 this->next = new Line(node);
+                cout<< *tam;
+                *tam++;
             }else{
-                this->next->in(node);
+                this->next->in(node, tam);
             }
         }
 
-        Vertice* out(){//tir do comeco
+        Vertice* out(int* tam){//tir do comeco
             Vertice* aux = this->next->val;
             this->next = this->next->next;
             return aux;
+            *tam--;
         }
 
-        void takeOff(Vertice* node){
+        void takeOff(Vertice* node, int* tam){
             if(this->next != nullptr && this->next->val->name == node->name){
                 this->next = this->next->next;
+                *tam--;
             }else{
-                this->next->takeOff(node);
+                this->next->takeOff(node, tam);
             }
         }
 
@@ -77,64 +85,64 @@ class SistemaOperacional{
 public:
     vector<Vertice> processos;//array de processos// um elemento desse array tem o nome do proceso e ua fila de recursos que ele está usando
     vector<Vertice> recursos;// array de recursos// um elemento desse assay tem o nome do recurso e a fila de processos que querem usar ele (ps: o primeiro é o que esta usando)
-    map<string, int> map;
+    map<string, int> m;
 
     SistemaOperacional(){
         this->processos;
         this->recursos;
-        this->map;
+        this->m;
     }
 
-    void req(string p, string r){//processo solicita recurso
-        if(map.count(p)&&map.count(r)){//se o processo e o recurso já existem
-            Vertice* recurso = &this->recursos[this->map[r]];
-            Vertice* processo = &this->processos[this->map[p]];
+    void req(string p, string r){ //processo solicita recurso
+        if(m.count(p)&&m.count(r)){//se o processo e o recurso já existem
+            Vertice* recurso = &this->recursos[this->m[r]];
+            Vertice* processo = &this->processos[this->m[p]];
 
             if(recurso->line->isFree()){//recurso esta livre // o processo consegue o recurso
-                recurso->line->in(processo);
-                processo->line->in(recurso);
+                recurso->line->in(processo, &recurso->line->size);
+                processo->line->in(recurso, &processo->line->size);
                 cout << "AVAIL" << endl;
             }else{//se o recurso esta sendo usado // o processo entra na fila do recurso
-                recurso->line->in(processo);
-                cout << "WAIT W" << endl;
+                recurso->line->in(processo, &recurso->line->size);
+                cout << "WAIT " << recurso->line->size << endl;
             }
 
-        }else if(map.count(p)){//se o processo ja existe
+        }else if(m.count(p)){//se o processo ja existe
             Vertice* recurso = new Vertice(r);
-            Vertice* processo = &this->processos[this->map[p]];
+            Vertice* processo = &this->processos[this->m[p]];
             recursos.push_back(*recurso);/////////////////////////////////////////////////////////// talvez aqui de problema com o apontador e etc
-            map[r] = recursos.size()-1;
+            m[r] = recursos.size()-1;
 
-            recurso->line->in(processo);
-            processo->line->in(recurso);
+            recurso->line->in(processo, &recurso->line->size);
+            processo->line->in(recurso, &processo->line->size);
 
             cout << "AVAIL" << endl;
 
-        }else if(map.count(r)){//se o recurso ja existe
-            Vertice* recurso = &this->recursos[this->map[r]];
+        }else if(m.count(r)){//se o recurso ja existe
+            Vertice* recurso = &this->recursos[this->m[r]];
             Vertice* processo = new Vertice(p);
             processos.push_back(*processo);
-            map[p] = processos.size()-1;
+            m[p] = processos.size()-1;
 
             if(recurso->line->isFree()){//recurso esta livre // o processo consegue o recurso
-                recurso->line->in(processo);
-                processo->line->in(recurso);
+                recurso->line->in(processo, &recurso->line->size);
+                processo->line->in(recurso, &processo->line->size);
                 cout << "AVAIL" << endl;
             }else{//se o recurso esta sendo usado // o processo entra na fila do recurso
-                recurso->line->in(processo);
-                cout << "WAIT W" << endl;
+                recurso->line->in(processo, &recurso->line->size);
+                cout << "WAIT " << recurso->line->size << endl;
             }
 
         }else{//se nenhum dos 2 existem
             Vertice* recurso = new Vertice(r);
             Vertice* processo = new Vertice(p);
             recursos.push_back(*recurso);
-            map[r] = recursos.size()-1;
+            m[r] = recursos.size()-1;
             processos.push_back(*processo);
-            map[p] = processos.size()-1;
+            m[p] = processos.size()-1;
 
-            recurso->line->in(processo);
-            processo->line->in(recurso);
+            recurso->line->in(processo, &recurso->line->size);
+            processo->line->in(recurso, &processo->line->size);
 
             cout << "AVAIL" << endl;
 
@@ -159,7 +167,9 @@ int main(int argc, char *argv[]) {
 
     SistemaOperacional* so = new SistemaOperacional();//ta dando um erro aqui
 
-   string func, p, r;
+    string func;
+    string p;
+    string r;
 
     cin >> func;
 
