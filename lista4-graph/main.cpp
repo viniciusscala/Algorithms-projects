06ef56cd35d60e1180d96f3b73294522bf63068a
Vinícius Scala <viniciusscala@Viniciuss-MacBook-Pro.local>
2019-10-29 01:20:17 -0300

    falta tirar o looping infinito do dlk

--- lista4-graph/main.cpp
+++ lista4-graph/main.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+int contG = 0;
+
 class Vertice{
 public:
 
@@ -83,32 +85,18 @@ public:
 
         }
 
+        Vertice* getSecond(){
+            if(this->next->next==nullptr){
+                return nullptr;
+            }else{
+                return this->next->next->val;
+            }
+
+        }
+
         void clear(){
             this->next = nullptr;
         }
-
-        void clear1(){
-            if(this->next!=nullptr){
-                this->next->next = nullptr;//quando ele cria uma copia, ele so cria a copia da cabeca, como o resto é apontador, ele usa o apontador do aux e do normal
-            }
-        }
-
-        Line clone(Line* aux){
-
-            //a fila sempre tem o primeiro mas ele n importa
-
-            if(this->next==nullptr){//clona o atual e para
-
-            }else{//clona o atual e da o passo recursivo
-                aux->val = this->val;
-                aux->next = this->next;//isso n vai dar certo, ele só vai copar o apontador
-
-                //eu tenho que sair dando new
-
-                this->next->clone(aux->next);
-            }
-
-        }//ele vai retornar a cabeça p a nova lista
 
     };
 
@@ -127,7 +115,7 @@ public:
     }
 
 
-//    bool deadLockAux(stack<Vertice*> pilha, stack<Vertice*> limpar, Vertice* parametro, bool teste, map<Vertice*, string> exist){
+//    bool deadLockAux(stack<Vertice*> pilha, stack<Vertice*> limpar, Vertice* parametro, bool teste, unordered_map<Vertice*, string> exist){
 //
 //        //processo -> usa o want
 //        //eu posso criar um vector de apontador de fila//ou talvez uma pilha
@@ -194,60 +182,89 @@ public:
 
     //o prox é r6
 
-    bool deadlockAux1(stack<Line*> pilha, Vertice* parametro, map<Vertice*, string> exist){//eu to adicionando o primeiro recurso sem verificar
+    bool deadlockAux1(stack<Line*> pilha, Vertice* parametro, unordered_map<Vertice*, string> exist, unordered_map<Vertice*, string> foi){//eu to adicionando o primeiro recurso sem verificar
+
+        contG++;
+
+        if(contG>1000){
+            cout<<"se fodeu kkkkkkkkkkkkkkkkkk"<<endl;
+        }
+
         if(this->name[0]=='P'){//se for processo
             if(!pilha.top()->isFree()){//se ainda tem coisa apontando
                 Vertice* proxR = pilha.top()->getFirst();//pega o primeiro da fila//tenho que parr de usar esse out
+
                 if(exist[proxR]=="grey"){
                     return true;
                 }else{
                     pilha.push(proxR->line);//bota o primeiro na fila
                     exist[proxR] = "grey";
-                    return proxR->deadlockAux1(pilha,parametro,exist);//faz recursivamente com o ultimo da pilha
+                    return proxR->deadlockAux1(pilha,parametro,exist,foi);//faz recursivamente com o ultimo da pilha
                 }
-//                pilha.push(proxR->line);//bota o primeiro na fila
-//                exist[proxR] = "grey";
-//                bool test1 = !(exist.count(proxR)&&exist[proxR]=="grey");
-//                bool test2 = exist.count(proxR);
-//                bool test3 = exist[proxR]=="grey";
-//                if(test2&&test3){//is free n funciona p recurso
-//                    return true;
-//                }else{
-//                    return proxR->deadlockAux1(pilha,parametro,exist);//faz recursivamente com o ultimo da pilha
-//                }
             }else{
                 pilha.pop();
                 exist[this] = "black";
+                foi[this]="vai meu pai";
                 if(pilha.size()==0){//se a pilha acabou, retorna falso
                     return false;
-                }else{//aqui tem que fazer a funcao recursova p que o ngm nunca retorne nullptr
-                    while(pilha.size()>0&&pilha.top()->next==nullptr){
+                }else{//aqui tem que fazer a funcao recursova p que o ngm nunca retorne nullptr//////////////////////////////ESSE ESTA CERTO
+                    int contador = 0;
+                    while(!pilha.empty()&&((pilha.size()>0&&pilha.top()->next->getFirst()==nullptr)||(pilha.size()>0&&pilha.top()->getFirst()->name[0]=='P'))){//tenho que adicionar mais uma condicao p ele sempre pular recursos
+                        contador++;
                         pilha.pop();
+                        if(pilha.size()>0){
+                            Line* saiu2 = pilha.top();//o get f desse daqui é o que eu preciso tirar
+                            exist[saiu2->getFirst()]="black";
+                            foi[saiu2->getFirst()]="vai meu pai";
+                        }
+                        if(contador>100){
+                            break;
+                        }
                     }
                     if(pilha.size()==0){//se a pilha acabou, retorna falso
                         return false;
-                    }else{
+                    }else{//ele n pode simplesmente voltar e ir p o prox pq o prox do recuro anterior n existe como aresta
+                        //pilha.pop();
                         Line* auxx = pilha.top();
                         pilha.pop();
                         auxx = auxx->next;
                         pilha.push(auxx);
-                        return pilha.top()->val->deadlockAux1(pilha, parametro, exist);//faz recursivamente com o ultimo da pilha
+                        Vertice* prox = pilha.top()->getFirst();
+                        if(prox->name[0]=='P'){
+                            pilha.push(prox->want);
+                        }else{
+                            pilha.push(prox->line);
+                        }
+                        if(exist[prox]=="grey"){
+                            return true;
+                        }else{
+                            return prox->deadlockAux1(pilha, parametro, exist, foi);//faz recursivamente com o ultimo da pilha // n posso mandar com esse get first n
+                        }
+                        //eu tenho que adicionar um a mais na pilha, p quando eu usar um a menos no ngc de enviar (no lugar do getf) ele mandar a msm coisa
                     }
                 }
             }
         }else{//se for recurso//nesse caso, ou ele aponta p um ou ele n aponta p ngm
 
-            if(exist[this->line->getFirst()]=="grey"){//se a fila já acabou
+            if(foi[this]=="foi"){//se a fila já acabou//achei o erro//eu n posso fazer essa comparacao c grey
                 pilha.pop();
                 exist[this] = "black";
-//                Vertice* aux1 = pilha.top()->val;
-//                pilha.pop();
-//                exist[aux1] = "black";
+                foi[this]="vai meu pai";
                 if(pilha.size()==0){//se a pilha acabou, retorna falso
                     return false;
                 }else{
-                    while(pilha.size()>0&&pilha.top()->next==nullptr){
+                    int contador = 0;
+                    while(!pilha.empty()&&((pilha.size()>0&&pilha.top()->next->getFirst()==nullptr))){
+                        contador++;
                         pilha.pop();
+                        if(pilha.size()>0){
+                            Line* saiu2 = pilha.top();//o get f desse daqui é o que eu preciso tirar
+                            exist[saiu2->getFirst()]="black";
+                            foi[saiu2->getFirst()]="vai meu pai";
+                        }
+                        if(contador>100){
+                            break;
+                        }
                     }
                     if(pilha.size()==0){//se a pilha acabou, retorna falso
                         return false;
@@ -256,26 +273,31 @@ public:
                         pilha.pop();
                         auxx = auxx->next;
                         pilha.push(auxx);
-                        return pilha.top()->val->deadlockAux1(pilha, parametro, exist);//faz recursivamente com o ultimo da pilha
+                        Vertice* prox = pilha.top()->getFirst();
+                        if(prox->name[0]=='P'){
+                            pilha.push(prox->want);
+                        }else{
+                            pilha.push(prox->line);
+                        }
+                        if(exist[prox]=="grey"){
+                            return true;
+                        }
+                        return prox->deadlockAux1(pilha, parametro, exist, foi);//faz recursivamente com o ultimo da pilha
                     }
                 }
             }else{//se tem prox
-                Vertice* proxP = pilha.top()->getFirst();//pega o primeiro da fila//tenho que parr de usar esse out
+                if(exist[this->line->getFirst()]=="grey"){
+                    return true;
+                }
+                foi[this]="foi";
+                Vertice* proxP = pilha.top()->getFirst();//pega o primeiro da fila//tenho que parr de usar esse out//ta dando problema aqui, ele ta pegando o mesmo que o this
                 if(exist[proxP]=="grey"){
                     return true;
                 }else{
                     pilha.push(proxP->want);//bota o primeiro na fila
                     exist[proxP] = "grey";
-                    return proxP->deadlockAux1(pilha,parametro,exist);//faz recursivamente com o ultimo da pilha
+                    return proxP->deadlockAux1(pilha,parametro,exist, foi);//faz recursivamente com o ultimo da pilha
                 }
-//                exist[aux] = "grey";
-//                pilha.push(aux->want);//bota o primeiro na fila
-//                Vertice* proxP = pilha.top()->getFirst();
-//                if(!proxP->want->isFree()&&exist.count(proxP->want->getFirst())&&exist[proxP->want->getFirst()]=="grey"){
-//                    return true;
-//                }else{
-//                    return proxP->deadlockAux1(pilha,parametro,exist);//faz recursivamente com o ultimo da pilha
-//                }
             }
         }
     }
@@ -287,10 +309,11 @@ public:
         //recurso usa o primeiro do line
 
         stack<Line*> pilha;
-        map<Vertice*, string> exist;
+        unordered_map<Vertice*, string> exist;
         exist.clear();
         pilha.push(this->want);
         exist[this] = "grey";
+        unordered_map<Vertice*, string> foi;
 
         //e se essa pilha for de apontador de fila?//nesse caso, ele acabaria quando o da base chegasse em null, pq eu iria dando next nele
 
@@ -300,7 +323,7 @@ public:
 
         //vou criar um metodo p clonar a fila
 
-        return this->deadlockAux1(pilha, this, exist);
+        return this->deadlockAux1(pilha, this, exist, foi);
 
     }
 };
@@ -309,27 +332,27 @@ class SistemaOperacional{
 public:
     vector<Vertice*> processos;//array de processos// um elemento desse array tem o nome do proceso e ua fila de recursos que ele está usando
     vector<Vertice*> recursos;// array de recursos// um elemento desse assay tem o nome do recurso e a fila de processos que querem usar ele (ps: o primeiro é o que esta usando)
-    map<string, int> m;
+    unordered_map<string, int> m;
 
 
     void req(string p, string r){ //processo solicita recurso
         if(m.count(p)&&m.count(r)){//se o processo e o recurso já existem
 //            Vertice** recurso = &this->recursos[this->m[r]];
 //            Vertice** processo = &this->processos[this->m[p]];
-
-            if(this->recursos[this->m[r]]->line->isFree()){//recurso esta livre // o processo consegue o recurso
-                this->recursos[this->m[r]]->line->in(this->processos[this->m[p]], &this->recursos[this->m[r]]->line->size);
-                this->processos[this->m[p]]->line->in(this->recursos[this->m[r]], &this->processos[this->m[p]]->line->size);
-                cout << "AVAIL" << endl;
-            }else{//se o recurso esta sendo usado // o processo entra na fila do recurso
-                this->recursos[this->m[r]]->line->in(this->processos[this->m[p]], &this->recursos[this->m[r]]->line->size);
-                this->processos[this->m[p]]->want->in(this->recursos[this->m[r]], &this->processos[this->m[p]]->want->size);
-                cout << "WAIT " << this->recursos[this->m[r]]->line->size << endl;
-            }
+            //if(!this->recursos[this->m[r]]->line->exist(this->processos[this->m[p]])){
+                if(this->recursos[this->m[r]]->line->isFree()){//recurso esta livre // o processo consegue o recurso
+                    this->recursos[this->m[r]]->line->in(this->processos[this->m[p]], &this->recursos[this->m[r]]->line->size);
+                    this->processos[this->m[p]]->line->in(this->recursos[this->m[r]], &this->processos[this->m[p]]->line->size);
+                    cout << "AVAIL" << endl;
+                }else{//se o recurso esta sendo usado // o processo entra na fila do recurso
+                    this->recursos[this->m[r]]->line->in(this->processos[this->m[p]], &this->recursos[this->m[r]]->line->size);
+                    this->processos[this->m[p]]->want->in(this->recursos[this->m[r]], &this->processos[this->m[p]]->want->size);
+                    cout << "WAIT " << this->recursos[this->m[r]]->line->size << endl;
+                }
+            //}
 
         }else if(m.count(p)){//se o processo ja existe
             Vertice* recurso = new Vertice(r);
-            Vertice** processo = &this->processos[this->m[p]];
             recursos.push_back(recurso);/////////////////////////////////////////////////////////// talvez aqui de problema com o apontador e etc// quando o vector aumenta de tamanho, a memoria das coisas mudam de lugar
             m[r] = recursos.size()-1;
 
@@ -339,20 +362,21 @@ public:
             cout << "AVAIL" << endl;
 
         }else if(m.count(r)){//se o recurso ja existe
-            Vertice** recurso = &this->recursos[this->m[r]];
             Vertice* processo = new Vertice(p);
             processos.push_back(processo);
             m[p] = processos.size()-1;
 
-            if(this->recursos[this->m[r]]->line->isFree()){//recurso esta livre // o processo consegue o recurso
-                this->recursos[this->m[r]]->line->in(processo, &this->recursos[this->m[r]]->line->size);
-                processo->line->in(this->recursos[this->m[r]], &processo->line->size);
-                cout << "AVAIL" << endl;
-            }else{//se o recurso esta sendo usado // o processo entra na fila do recurso
-                this->recursos[this->m[r]]->line->in(processo, &this->recursos[this->m[r]]->line->size);
-                processo->want->in(this->recursos[this->m[r]], &processo->want->size);
-                cout << "WAIT " << this->recursos[this->m[r]]->line->size << endl;
-            }
+            //if(!this->recursos[this->m[r]]->line->exist(this->processos[this->m[p]])){
+                if(this->recursos[this->m[r]]->line->isFree()){//recurso esta livre // o processo consegue o recurso
+                    this->recursos[this->m[r]]->line->in(processo, &this->recursos[this->m[r]]->line->size);
+                    processo->line->in(this->recursos[this->m[r]], &processo->line->size);
+                    cout << "AVAIL" << endl;
+                }else{//se o recurso esta sendo usado // o processo entra na fila do recurso
+                    this->recursos[this->m[r]]->line->in(processo, &this->recursos[this->m[r]]->line->size);
+                    processo->want->in(this->recursos[this->m[r]], &processo->want->size);
+                    cout << "WAIT " << this->recursos[this->m[r]]->line->size << endl;
+                }
+            //}
 
         }else{//se nenhum dos 2 existem
             Vertice* recurso = new Vertice(r);
@@ -375,6 +399,9 @@ public:
             int cont = 0;
             while(processos[m[p]]->line->size!=-1){//enquanto ainda tiver coisa na fila de recursos que o processo tem
                 Vertice* recurso = processos[m[p]]->line->out(&processos[m[p]]->line->size);//tira um recurso da fila de recursos que o processo tem
+                if(recurso==nullptr){
+                    break;
+                }
                 Vertice* processo = recurso->line->out(&recurso->line->size);//faz o recurso parar de apontar para o processo
                 if(!recurso->line->isFree()){//se tem coisa na fila do recurso
                     processo = recurso->line->getFirst();//pega que coisa é essa
@@ -382,6 +409,20 @@ public:
                     processo->want->takeOff(recurso, &processo->want->size);
                 }
                 cont++;
+                if(cont>100){
+                    break;
+                }
+            }
+            while(processos[m[p]]->want->size!=-1){
+                Vertice* recurso = processos[m[p]]->want->out(&processos[m[p]]->want->size);
+                if(recurso==nullptr){
+                    break;
+                }
+                recurso->line->takeOff(processos[m[p]], &recurso->line->size);
+                cont++;
+                if(cont>200){
+                    break;
+                }
             }
             processos[m[p]]->want->clear();
             cout << "TERM " << cont << endl;
@@ -389,21 +430,42 @@ public:
     }
 
     void dlk(string p){
-        if(processos[m[p]]->deadlock()){//se tiver deadlock
+        if(m.count(p)&&processos[m[p]]->deadlock()){//se tiver deadlock
             if(m.count(p)){//se p existe
                 int cont = 0;
                 int contR = 0;
                 while(processos[m[p]]->line->size!=-1){
+                    //recurso que o this tinha
                     Vertice* recurso = processos[m[p]]->line->out(&processos[m[p]]->line->size);//se aqui só tira os que ele ja tem, o recurso sempre vai estar em uso
+                    if(recurso==nullptr){
+                        break;
+                    }
                     //tenho que ver se o processo já estava com o recurso ou nao
+                    //processo que o recurso vai apontar agr
                     Vertice* processo = recurso->line->out(&recurso->line->size);
-                    if(!recurso->line->isFree()){
+                    if(!recurso->line->isFree()){//se tem alguma coisa p passar
                         processo = recurso->line->getFirst();
                         processo->line->in(recurso, &processo->line->size);
+                        processo->want->takeOff(recurso, &processo->want->size);
                     }
                     cont++;
+                    if(cont>100){
+                        break;
+                    }
                 }
-                contR = processos[m[p]]->want->size;
+                while(processos[m[p]]->want->size!=-1){
+                    Vertice* recurso = processos[m[p]]->want->out(&processos[m[p]]->want->size);
+                    if(recurso==nullptr){
+                        break;
+                    }
+                    recurso->line->takeOff(processos[m[p]], &recurso->line->size);
+                    contR++;
+                    if(contR>100){
+                        break;
+                    }
+                }
+               //contR = processos[m[p]]->want->size;
+                //contR = contR + 1;
                 cout << "KILL " << cont << " " << contR << endl;
             }
         }else{
@@ -427,8 +489,11 @@ int main(int argc, char *argv[]) {
 
     cin >> func;
 
-    while(func!="END"){
+    int cont = 0;
 
+    while(func!="END"){
+        cont++;
+        //cout << cont << endl;
         if(func == "REQ"){
             cin >> p;
             cin >> r;
